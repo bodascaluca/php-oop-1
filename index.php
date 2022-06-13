cfe9ed551cacf9be7d5755665f650ccdfff72fd5
bodascaluca <bodasca.luca@gmail.com>
2022-06-13 22:29:07 +0200

    Abbiamo aggiunto la maggiore età

--- index.php
+++ index.php
@@ -13,11 +13,13 @@
         public $name;
         public $genere;
         public $voti = [];
+        public $maggiorenne;
 
-        function __construct($_name, $_genere)
+        function __construct($_name, $_genere, $_maggiorenne)
         {
           $this->name = $_name;
           $this->genere = $_genere;
+          $this->maggiorenne = $_maggiorenne;
         }
 
         public function insertVoto($voto)
@@ -32,14 +34,28 @@
             return $sum / count($this->voti);
         }
 
+        public function max()
+        {
+            if( $this->maggiorenne >= 18){
+               $result = "Film per maggiorenni";
+            } else {
+              $result = "Film per monorenni";
+            }
+            return $result; 
+        }
+
         public function getFullName(){
-            return "Il nome del film è" . $this->name . "il genere è" . $this->genere . "<br>" . "Il voto medio è " . $this->mediaVoto() . "su 5, è un film";
+            return "Il nome del film è" . $this->name . "il genere è" . $this->genere . "<br>" . "Il voto medio è " . $this->mediaVoto() . "su 5, è un film" . $this->max();
+        }
+
+        public function maggiorenne__no(){
+            return $this->max();
         }
     }
 
-    $film_1 = new Movie ("Jurasic World:Il Dominio", "Aventuroso");
-    $film_2 = new Movie ("Top Gun: Maverick", "Azione");
-    $film_3 = new Movie ("Nostalgia", "Amoroso");
+    $film_1 = new Movie ("Jurasic World:Il Dominio", "Aventuroso", 18);
+    $film_2 = new Movie ("Top Gun: Maverick", "Azione", 17);
+    $film_3 = new Movie ("Nostalgia", "Amoroso", 21);
 
     $film_1->insertVoto(4);
     $film_1->insertVoto(5);
